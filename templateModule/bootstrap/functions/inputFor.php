fcd110a15852fba3e9a6ea0079b60c5149cbded7
Mark Smit <m.smit@hostingondemand.nl>
2016-10-20 23:02:38 +0200

    minor change for framework update

--- templateModule/bootstrap/functions/inputFor.php
+++ templateModule/bootstrap/functions/inputFor.php
@@ -8,7 +8,7 @@ class FuncInputFor extends \lib\template\AbstractFunction
 
     function call($parameters, $data, $content = "", $unparsed = "",$module=false)
     {
-        return $this->template->parseFile("bootstrap/inputFor",array_merge($data,array("_field"=>$parameters[0],"_type"=>$parameters[1],"_source"=>isset($parameters[2])?$parameters[2]:array())));
+        return $this->template->parseFile("bootstrap/inputFor",$data->set(array("_field"=>$parameters[0],"_type"=>$parameters[1],"_source"=>isset($parameters[2])?$parameters[2]:array())));
     }
 
 
