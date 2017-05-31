0b5fddfd3f67e27db22e3db6b3bf4172e8d74c36
Mark Smit <m.smit@hostingondemand.nl>
2017-05-31 16:39:00 +0200

    CC-84--bootstrap fix

--- clientmodule/bootstrap.php
+++ clientmodule/bootstrap.php
@@ -10,7 +10,15 @@ class Bootstrap extends BaseClientmodule
     {
         $this->service->client->addScript("bootstrap/js/bootstrap.js", "hodclient", 9990);
         $this->service->client->addStylesheet("bootstrap/css/bootstrap.css", "hodclient", 9989);
-        $this->service->client->addStylesheet("bootstrap/css/bootstrap-theme.css", "hodclient", 9988);
+
+
+        if($this->filesystem->findRightPath("content/css/bootstrap-theme.css")) {
+            $this->service->client->addStylesheet("css/bootstrap-theme.css", false, 9988);
+        } elseif($this->filesystem->findRightPath("content/style/bootstrap-theme.css")){
+                $this->service->client->addStylesheet("style/bootstrap-theme.css", false, 9988);
+        }else {
+            $this->service->client->addStylesheet("bootstrap/css/bootstrap-theme.css", "hodclient", 9988);
+        }
         $this->service->client->addStylesheet("bootstrap/css/custom.css", "hodclient", 9988);
     }
 }
